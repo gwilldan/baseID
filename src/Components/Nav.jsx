719042a5d6447d15c280112d0f166cdd0c840823
gwilldan <sayhi2godswill@gmail.com>
2023-08-14 17:49:52 +0100

    fixed switch button

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { darkMobile, Logo } from '../Assets'
-import {Button} from "../Components"
+import React, {useState} from 'react'
+import { Logo } from '../Assets'
+import {Button, Switch} from "../Components"
 import {FiMoon} from "react-icons/fi"
 
 function Nav() {
+
   return (
     <div 
     className=' h-[100px] border-b border-priBlue flex justify-between pt-[40px] px-small md:px-[40px] lg:px-Large
@@ -13,8 +14,14 @@ function Nav() {
         <div className=' flex items-center'>
         <img src={Logo} alt="logo" className=" md:h-8 " />
         </div>
-        <div className=' flex gap-2 items-center'>
+
+        
+
+        <div className=' flex gap-4 items-center'>
             <button className=''><FiMoon className=' text-2xl' /></button>
+          { 
+            <Switch />
+          }
             <Button />
         </div>
     </div>
