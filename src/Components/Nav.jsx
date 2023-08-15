9a9ebad636aac7faabf467f68c643349a8bf552a
gwilldan <sayhi2godswill@gmail.com>
2023-08-15 02:06:21 +0100

    fixed darkmode bug

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,19 +1,9 @@
-import React, {useState, useEffect} from 'react'
-import { Logo } from '../Assets'
-import {Button, Switch} from "../Components"
+import React from 'react'
+import { Logo, LogoDark } from '../Assets'
+import {Button} from "../Components"
 import {FiMoon, FiSun} from "react-icons/fi"
 
-function Nav() {
-
-  const [theme, setTheme] = useState("light")
-  useEffect(() => {
-    if(theme === "dark"){
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  }, [theme])
-
+function Nav({theme, setTheme}) {
 
   return (
     <div 
@@ -22,11 +12,8 @@ function Nav() {
     '
     >
         <div className=' flex items-center'>
-        <img src={Logo} alt="logo" className=" md:h-8 " />
+        <img src={theme === "light" ? Logo : LogoDark} alt="logo" className=" md:h-8 " />
         </div>
-
-        
-
         <div className=' flex gap-4 items-center'>
             <button className='' onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
               { theme === "light" ? <FiMoon className=' text-2xl' /> : <FiSun className=' text-2xl text-white' />} 
