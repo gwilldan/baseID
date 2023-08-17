f28615ba884dd2911bde6072d4e4e60774016a46
gwilldan <sayhi2godswill@gmail.com>
2023-08-17 06:06:41 +0100

    finished

--- src/Pages/DMail.jsx
+++ src/Pages/DMail.jsx
@@ -14,7 +14,7 @@ function DMail() {
     >
 
         <div className=' md:flex gap-5'>
-            <div className=' md:h-[350px] max-w-[650px]: bg-secondary-color py-10 px-5' >
+            <div className=' md:mt-[30px] md:h-[350px] max-w-[650px]: bg-secondary-color py-10 px-5' >
               <h1 className='  --accent-color text font-extrabold text-3xl md:text-7xl mb-2'>Harness your ID <br /> with D-Mail</h1>
               <p className='text-left mt-6 font-normal md:text-xl '>
                 "Introducing our Blockchain Mail Service, where secure identities from our Blockchain Name Service converge with encrypted communications, ensuring your messages are safe and trusted."
