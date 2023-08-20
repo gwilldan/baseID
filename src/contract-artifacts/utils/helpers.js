2af2ab41999fc471bb0eaf65a7471ef2313171bd
ricqCodes <princenwakanma1996@gmail.com>
2023-08-20 17:02:15 +0100

    added

--- src/contract-artifacts/utils/helpers.js
+++ src/contract-artifacts/utils/helpers.js
@@ -1,43 +1,13 @@
 import axios from "axios";
 
-// export const getUserDomainNames = async (user, options) => {
-//   if (!user) return;
-
-//   const rpc = chains[options.network].rpc;
-//   console.log(rpc);
-//   console.log(ANKRPC_GOERLI);
-
-//   // Create provider
-//   const provider = new ethers.JsonRpcProvider(rpc);
-//   console.log(provider);
-//   const contract = new ethers.Contract(CA, abi, provider);
-
-//   const eventFilter = contract.filters.DomainRegistered(user, null, null);
-//   const logs = await contract.queryFilter(eventFilter, FROM_BLOCK);
-
-//   let domains = [];
-//   if (options) {
-//     domains = logs.map((log, i) => {
-//       return {
-//         id: i + 1,
-//         domain: log.args.domainName,
-//         controller: options.selectedName === log.args.domainName, // CORRECT WAY IF CONTRACT MAKES PROVISIONS
-//       };
-//     });
-//   } else {
-//     domains = logs.map((log) => log.args.domainName);
-//   }
-
-//   return domains;
-// };
+const BACKENDURL =
+  import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_TEST_BACKEND_URL;
 
 export const getUserDomainNames = async (user, options) => {
   let domains;
   console.log(typeof options?.chainId);
   if (options) {
-    domains = await axios.get(
-      `http://localhost:9000/api/user-domains/${user}/${options.chainId}`
-    );
+    domains = await axios.get(`${BACKENDURL}/${user}/${options.chainId}`);
   }
   return domains.data;
 };
